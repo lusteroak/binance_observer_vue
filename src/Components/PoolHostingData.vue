278c454dda213aa1955e48418e1f702a5812ba72
lusteroak <lusteroak@protonmail.com>
2023-08-07 13:36:02 -0400

    fix styles

--- src/Components/PoolHostingData.vue
+++ src/Components/PoolHostingData.vue
@@ -31,25 +31,25 @@ onUnmounted(() => clearInterval(refresh))
 <template>
     <div class="grid">
         <div v-for="data in getFullData" :key="data" class="col-12 sm:col-6 lg:col-4 lg:m-0 xl:col-3">
-            <Card class="flex flex-column align-items-center xl:flex-row text-sm md:text-lg">
+            <Card class="flex flex-column align-items-center text-sm md:text-lg">
                 <template #title>
-                    <p>{{ data[0].data.userHash.userName }}</p>
+                    <p class="text-center">{{ data[0].data.userHash.userName }}</p>
                 </template>
                 <template #subtitle>
-                    <p>Hashrate: {{ hashRateFormatted(data[0].data.userHash.value) }}</p>
+                    <p class="text-center">Hashrate: {{ hashRateFormatted(data[0].data.userHash.value) }}</p>
                 </template>
                 <template #content>
                     <div v-if="data[1].data.invalidNum" class="flex justify-content-evenly">
                         <Badge size="large" severity="success" text raised>
-                            <p>{{ data[1].data.validNum }}</p>
+                            <p class="text-center">{{ data[1].data.validNum }}</p>
                         </Badge>
                         <Badge size="large" severity="danger" text raised>
-                            <p>{{ data[1].data.invalidNum }}</p>
+                            <p class="text-center">{{ data[1].data.invalidNum }}</p>
                         </Badge>
                     </div>
                     <div v-else class="flex justify-content-center">
                         <Badge size="large" severity="success" text raised>
-                            <p> {{ data[1].data.validNum }}</p>
+                            <p class="text-center"> {{ data[1].data.validNum }}</p>
                         </Badge>
                     </div>
                 </template>
