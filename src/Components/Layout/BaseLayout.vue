b1434763e41b5415f198db007c54847b93b67471
lusteroak <lusteroak@protonmail.com>
2023-08-07 14:52:38 -0400

    fix styles

--- src/Components/Layout/BaseLayout.vue
+++ src/Components/Layout/BaseLayout.vue
@@ -9,52 +9,50 @@ const showMenu = ref(false)
 <template>
     <div class="flex bg-blue-900 overflow-x-hidden">
         <div id="app-sidebar-5" :class="showMenu ? '' : 'hidden'"
-                class="relative sm:block sm:col-3 md:col-2 bg-gray-800 p-component flex-shrink-0 border-gray-800 select-none">
+            class="relative col-4 sm:block sm:col-3 md:col-2 bg-gray-800 p-component flex-shrink-0 border-gray-800 select-none">
 
-                <div class="flex flex-column align-items-center w-10 lg:h-full">
-                    <div class="flex align-items-center justify-content-center flex-shrink-0" style="height: 60px;"><img
-                            src="https://upload.wikimedia.org/wikipedia/commons/4/46/Bitcoin.svg" alt="Image" height="30">
-                    </div>
-                    <div>
-                        <ul class="list-none p-0 sm:p-2 lg:p-3">
-                            <router-link to="/" class="no-underline">
-                                <li><a
-                                        class="flex flex-row lg:flex-column align-items-center cursor-pointer p-1 lg:justify-content-center hover:bg-gray-800 border-round text-gray-300 hover:text-white transition-duration-150 transition-colors p-ripple"><i
-                                            class="pi pi-home mr-2 lg:mr-0 mb-0 lg:mb-2 text-base lg:text-lg"></i><span
-                                            class="font-medium inline text-base lg:text-xs lg:block">Dashboard</span><span
-                                            class="p-ink" role="presentation" aria-hidden="true"></span></a></li>
-                            </router-link>
-                            <router-link to="/pool" class="no-underline">
-                                <li><a
-                                        class="flex flex-row lg:flex-column align-items-center cursor-pointer p-1 lg:justify-content-center hover:bg-gray-800 border-round text-gray-300 hover:text-white transition-duration-150 transition-colors p-ripple"><i
-                                            class="pi pi-search mr-2 lg:mr-0 mb-0 lg:mb-2 text-base lg:text-lg"></i><span
-                                            class="font-medium inline text-base lg:text-xs lg:block">Pool</span><span
-                                            class="p-ink" role="presentation" aria-hidden="true"></span></a></li>
-                            </router-link>
-                            <router-link to="/ganancias" class="no-underline">
-                                <li><a
-                                        class="flex flex-row lg:flex-column align-items-center cursor-pointer p-1 lg:justify-content-center hover:bg-gray-800 border-round text-gray-300 hover:text-white transition-duration-150 transition-colors p-ripple"><i
-                                            class="pi pi-users mr-2 lg:mr-0 mb-0 lg:mb-2 text-base lg:text-lg"></i><span
-                                            class="font-medium inline text-base lg:text-xs lg:block">Ganancias</span><span
-                                            class="p-ink" role="presentation" aria-hidden="true"></span></a></li>
-                            </router-link>
+            <div class="flex flex-column lg:h-full">
+                <div class="flex align-items-center justify-content-center flex-shrink-0" style="height: 60px;"><img
+                        src="https://upload.wikimedia.org/wikipedia/commons/4/46/Bitcoin.svg" alt="Image" height="30">
+                </div>
+                <div>
+                    <ul class="list-none p-0 sm:p-2 lg:p-3">
+                        <router-link to="/" class="no-underline">
+                            <li><a
+                                    class="flex flex-row lg:flex-column cursor-pointer p-1 lg:justify-content-center hover:bg-gray-800 border-round text-gray-300 hover:text-white transition-duration-150 transition-colors p-ripple">
+                                    <span
+                                        class="font-medium inline text-sm sm:text-xl lg:text-2xl lg:block">Dashboard</span></a>
+                            </li>
+                        </router-link>
+                        <router-link to="/pool" class="no-underline">
+                            <li><a
+                                    class="flex flex-row lg:flex-column cursor-pointer p-1 lg:justify-content-center hover:bg-gray-800 border-round text-gray-300 hover:text-white transition-duration-150 transition-colors p-ripple"><span
+                                        class="font-medium inline text-sm sm:text-xl lg:text-2xl lg:block">Pool</span><span
+                                        class="p-ink" role="presentation" aria-hidden="true"></span></a></li>
+                        </router-link>
+                        <router-link to="/ganancias" class="no-underline">
+                            <li><a
+                                    class="flex flex-row lg:flex-column cursor-pointer p-1 lg:justify-content-center hover:bg-gray-800 border-round text-gray-300 hover:text-white transition-duration-150 transition-colors p-ripple"><span
+                                        class="font-medium inline text-sm sm:text-xl lg:text-2xl lg:block">Ganancias</span><span
+                                        class="p-ink" role="presentation" aria-hidden="true"></span></a></li>
+                        </router-link>
 
-                            <router-link to="/mineros" class="no-underline">
+                        <!-- <router-link to="/mineros" class="no-underline">
                                 <li><a
                                         class="flex flex-row lg:flex-column align-items-center cursor-pointer p-1 lg:justify-content-center hover:bg-gray-800 border-round text-gray-300 hover:text-white transition-duration-150 transition-colors p-ripple">
                                         <i
                                             class="pi pi-chart-line mr-2 lg:mr-0 mb-0 lg:mb-2 text-base lg:text-lg p-overlay-badge"></i><span
-                                            class="font-medium inline text-base lg:text-xs lg:block">Mineros</span><i
+                                            class="font-medium inline text-sm sm:text-xl lg:text-xs lg:block">Mineros</span><i
                                             class="pi pi-chevron-down ml-auto lg:hidden"></i><span class="p-ink"
                                             role="presentation" aria-hidden="true"></span></a>
                                 </li>
-                            </router-link>
-                        </ul>
-                    </div>
+                            </router-link> -->
+                    </ul>
                 </div>
             </div>
+        </div>
         <div class="min-h-screen w-full flex sm:block lg:static bg-blue-800 m-0">
-            
+
             <div class="min-h-full w-full flex sm:block">
                 <div class="flex md:block p-1">
                     <router-view />
