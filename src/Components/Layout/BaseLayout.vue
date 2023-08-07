d1e736a114ff379f7a11e5a461f2b7a9fdb3be5d
lusteroak <lusteroak@protonmail.com>
2023-08-07 15:34:30 -0400

    add active link styles in sidebar

--- src/Components/Layout/BaseLayout.vue
+++ src/Components/Layout/BaseLayout.vue
@@ -8,8 +8,8 @@ const showMenu = ref(false)
 
 <template>
     <div class="flex bg-blue-900 overflow-x-hidden">
-        <div id="app-sidebar-5" :class="showMenu ? '' : 'hidden'"
-            class="relative col-4 sm:block sm:col-3 md:col-2 bg-gray-800 p-component flex-shrink-0 border-gray-800 select-none">
+        <div id="app-sidebar-5" :class="showMenu ? 'fadeinleft animation-duration-500' : 'hidden'"
+            class="relative col-4 sm:block sm:col-3  md:col-2 bg-gray-800 p-component flex-shrink-0 border-gray-800 select-none">
 
             <div class="flex flex-column lg:h-full">
                 <div class="flex align-items-center justify-content-center flex-shrink-0" style="height: 60px;"><img
@@ -19,27 +19,27 @@ const showMenu = ref(false)
                     <ul class="list-none p-0 sm:p-2 lg:p-3">
                         <router-link to="/" class="no-underline">
                             <li><a
-                                    class="flex flex-row lg:flex-column cursor-pointer p-1 lg:justify-content-center hover:bg-gray-800 border-round text-gray-300 hover:text-white transition-duration-150 transition-colors p-ripple">
+                                    class="flex flex-row lg:flex-column cursor-pointer p-1 lg:justify-content-center hover:bg-gray-800 border-round hover:text-white transition-duration-150 transition-colors p-ripple">
                                     <span
                                         class="font-medium inline text-sm sm:text-xl lg:text-2xl lg:block">Dashboard</span></a>
                             </li>
                         </router-link>
                         <router-link to="/pool" class="no-underline">
                             <li><a
-                                    class="flex flex-row lg:flex-column cursor-pointer p-1 lg:justify-content-center hover:bg-gray-800 border-round text-gray-300 hover:text-white transition-duration-150 transition-colors p-ripple"><span
+                                    class="flex flex-row lg:flex-column cursor-pointer p-1 lg:justify-content-center hover:bg-gray-800 border-round hover:text-white transition-duration-150 transition-colors p-ripple"><span
                                         class="font-medium inline text-sm sm:text-xl lg:text-2xl lg:block">Pool</span><span
                                         class="p-ink" role="presentation" aria-hidden="true"></span></a></li>
                         </router-link>
                         <router-link to="/ganancias" class="no-underline">
                             <li><a
-                                    class="flex flex-row lg:flex-column cursor-pointer p-1 lg:justify-content-center hover:bg-gray-800 border-round text-gray-300 hover:text-white transition-duration-150 transition-colors p-ripple"><span
+                                    class="flex flex-row lg:flex-column cursor-pointer p-1 lg:justify-content-center hover:bg-gray-800 border-round hover:text-white transition-duration-150 transition-colors p-ripple"><span
                                         class="font-medium inline text-sm sm:text-xl lg:text-2xl lg:block">Ganancias</span><span
                                         class="p-ink" role="presentation" aria-hidden="true"></span></a></li>
                         </router-link>
 
                         <!-- <router-link to="/mineros" class="no-underline">
                                 <li><a
-                                        class="flex flex-row lg:flex-column align-items-center cursor-pointer p-1 lg:justify-content-center hover:bg-gray-800 border-round text-gray-300 hover:text-white transition-duration-150 transition-colors p-ripple">
+                                        class="flex flex-row lg:flex-column align-items-center cursor-pointer p-1 lg:justify-content-center hover:bg-gray-800 border-round hover:text-white transition-duration-150 transition-colors p-ripple">
                                         <i
                                             class="pi pi-chart-line mr-2 lg:mr-0 mb-0 lg:mb-2 text-base lg:text-lg p-overlay-badge"></i><span
                                             class="font-medium inline text-sm sm:text-xl lg:text-xs lg:block">Mineros</span><i
@@ -65,4 +65,16 @@ const showMenu = ref(false)
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.router-link-active > li > a > span{
+    color: #d5a326 !important;
+}
+
+.router-link-active > li > a > span{
+    color: #d5a326 !important;
+    font-weight: bold;
+}
+a:any-link {
+    color: white;
+}
+</style>
